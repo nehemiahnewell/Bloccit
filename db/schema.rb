a839328a71f2d13219a4f3dfaee39150b4376096
nehemiahnewell <nehemiah.newell@gmail.com>
2016-04-04 02:48:38 +0000

    setup

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160401022331) do
 
-  create_table "advertisements", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "answers", force: :cascade do |t|
     t.text     "body"
     t.integer  "question_id"

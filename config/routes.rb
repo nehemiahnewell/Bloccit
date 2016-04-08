7f25878c150f280c63ade3ba95b1aba5d1ff329f
nehemiahnewell <nehemiah.newell@gmail.com>
2016-04-08 00:34:23 +0000

    42-labels-checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,11 @@ Rails.application.routes.draw do
   resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
   end
+  resources :labels, only: [:show]
     
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+    
     
   get 'about' => 'welcome#about'
   root 'welcome#index'
--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -5,8 +5,10 @@
    let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
    let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
    let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
+
    let(:label) { Label.create!(name: 'Label') }
-   
+   let(:label2) { Label.create!(name: 'Label2') }
+
    it { is_expected.to have_many :labelings }
    it { is_expected.to have_many(:topics).through(:labelings) }
    it { is_expected.to have_many(:posts).through(:labelings) }
@@ -18,6 +20,13 @@
        topic_label = topic.labels[0]
        post_label = post.labels[0]
        expect(topic_label).to eql(post_label)
+     end
+   end
+   describe ".update_labels" do
+     it "takes a comma delimited string and returns an array of Labels" do
+       labels = "#{label.name}, #{label2.name}"
+       labels_as_a = [label, label2]
+       expect(Label.update_labels(labels)).to eq(labels_as_a)
      end
    end
  end

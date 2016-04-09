52e3d0464717089e49c6d6585640b70e71e86978
nehemiahnewell <nehemiah.newell@gmail.com>
2016-04-09 20:23:32 +0000

    40-authorization-assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
   
   before_action :require_sign_in, except: :show
-  before_action :authorize_user, except: [:show, :new, :create]
-
+  before_action :authorize_user, except: [:show, :new, :create, :destroy]
+  before_action :destruction_permission, only: [:destroy]
   
   def show
     @post = Post.find(params[:id])
@@ -64,6 +64,13 @@ class PostsController < ApplicationController
      params.require(:post).permit(:title, :body)
    end
    def authorize_user
+     post = Post.find(params[:id])
+     unless current_user == post.user || current_user.admin? || current_user.moderator?
+       flash[:alert] = "You must be an admin or moderator to do that."
+       redirect_to [post.topic, post]
+     end
+   end
+   def destruction_permission
      post = Post.find(params[:id])
      unless current_user == post.user || current_user.admin?
        flash[:alert] = "You must be an admin to do that."
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,6 +2,7 @@ class TopicsController < ApplicationController
 
     before_action :require_sign_in, except: [:index, :show]
     before_action :authorize_user, except: [:index, :show]
+    before_action :admin_user, only: [:new, :create, :destroy]
     
     def index
         @topics = Topic.all
@@ -62,6 +63,13 @@ class TopicsController < ApplicationController
    end
    
    def authorize_user
+     unless current_user.admin? || current_user.moderator?
+       flash[:alert] = "You must be an admin or moderator to do that."
+       redirect_to topics_path
+     end
+   end
+   
+   def admin_user
      unless current_user.admin?
        flash[:alert] = "You must be an admin to do that."
        redirect_to topics_path

8d0249295e557a209ee3c3ffef5eec984e744458
nehemiahnewell <nehemiah.newell@gmail.com>
2016-04-11 16:39:41 +0000

    partial

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,7 @@ class SessionsController < ApplicationController
   end
  
   def create
- # #1
     user = User.find_by(email: params[:session][:email].downcase)
-
- # #2
     if user && user.authenticate(params[:session][:password])
       create_session(user)
       flash[:notice] = "Welcome, #{user.name}!"
@@ -18,7 +15,6 @@ class SessionsController < ApplicationController
   end
  
   def destroy
- # #3
     destroy_session(current_user)
     flash[:notice] = "You've been signed out, come back soon!"
     redirect_to root_path

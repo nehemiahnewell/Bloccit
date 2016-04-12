009170356adad0c5ea9a581d4999cd774e821cb6
nehemiahnewell <nehemiah.newell@gmail.com>
2016-04-12 21:19:47 +0000

    clean up

--- spec/models/labeling_spec.rb
+++ spec/models/labeling_spec.rb
@@ -1,6 +1,5 @@
  require 'rails_helper'
  
  RSpec.describe Labeling, type: :model do
- # #6
    it { is_expected.to belong_to :labelable }
  end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-    let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
     
-    it { is_expected.to have_many(:posts) }
-    it { is_expected.to have_many(:labelings) }
-    it { is_expected.to have_many(:labels).through(:labelings) }
+  it { is_expected.to have_many(:posts) }
+  it { is_expected.to have_many(:labelings) }
+  it { is_expected.to have_many(:labels).through(:labelings) }
     
-    describe "attributes" do
-        it "responds to name" do
+  describe "attributes" do
+    it "responds to name" do
             expect(topic).to respond_to(:name)
-        end
-        it "responds to description" do
-            expect(topic).to respond_to(:description)
-        end
- 
-        it "responds to public" do
-            expect(topic).to respond_to(:public)
-        end
-        it "is public by default" do
-            expect(topic.public).to be(true)
-        end
     end
+    it "responds to description" do
+      expect(topic).to respond_to(:description)
+    end
+ 
+    it "responds to public" do
+      expect(topic).to respond_to(:public)
+    end
+    it "is public by default" do
+      expect(topic.public).to be(true)
+    end
+  end
 end

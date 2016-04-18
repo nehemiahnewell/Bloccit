b21159f82c67b41bd37344cb30984e2866a34c62
nehemiahnewell <nehemiah.newell@gmail.com>
2016-04-18 09:42:44 +0000

    broken

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show, :create, :update]
-      resources :topics, except: [:edit, :new]
+      resources :topics, except: [:edit, :new] do
+        resources :posts
+      end
     end
   end
 end

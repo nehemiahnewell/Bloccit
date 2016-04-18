9e9f270df5769203228e81296707fd940b2ce9df
nehemiahnewell <nehemiah.newell@gmail.com>
2016-04-18 07:02:31 +0000

    48-api-assignment

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CommentsController < Api::V1::BaseController
-  before_action :authenticate_user
-  before_action :authorize_user
+  before_action :authenticate_user, except: [:index, :show]
+  before_action :authorize_user, except: [:index, :show]
  
   def show
     comment = Comment.find(params[:id])
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PostsController < Api::V1::BaseController
-  before_action :authenticate_user
-  before_action :authorize_user
+  before_action :authenticate_user, except: [:index, :show]
+  before_action :authorize_user, except: [:index, :show]
  
   def show
     post = Post.find(params[:id])
